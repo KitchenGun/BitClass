57de2867d7a2f2a41f7faab5c84da65c9c3d5cff
KitchenGun <kitchengun124@gmail.com>
2020-06-08 20:35:38 +0900

    Java 시작

---
+++ Java/HelloWorld/FirstSample.java
@@ -0,0 +1,21 @@
+/*
+파일명 : 반드시 클래스명과 동일해야 함(문법)
+            FirstSample.java  
+설   명 : Hello, World! 출력
+           Java의 모든 코드는 클래스 내부에 존재함.
+           main 함수 조차도 클래스 내부에 있음.	
+           - static : 클래스맴버(객체 없이 클래스명으로 사용
+*/
+
+
+public class FirstSample
+{
+	public static void main(String[] args) {
+		System.out.println("Hello, World!\n");
+	}
+}
+
+class SecondSample 
+{
+	public void Foo() { } 
+}
