177850c07b56f1dca9344df7dcbae38c7b9c9b07
KitchenGun <kitchengun124@gmail.com>
2020-06-19 00:31:50 +0900

    자바 관리자 프로그램 복습

---
+++ Java/java2/Java6/src/woosongbit/sample/Student.java
@@ -0,0 +1,50 @@
+package woosongbit.sample;
+
+public class Student 
+{
+	//맴버
+	private int number;
+	private String name;
+	private int c;
+	private int cpp;
+	private float average;
+
+	private static int s_number=1000;
+	
+	//생성자
+	public Student(String name)
+	{
+		number=s_number;
+		this.name=name;
+		this.c=0;
+		this.cpp=0;
+		this.average=0;
+		s_number+=10;
+	}
+	
+	//get&set
+	public String getName()
+	{
+		return name;
+	}
+	
+	//메서드
+	public final void Println()
+	{
+		System.out.println("번호"+number);
+		System.out.println("이름"+name);
+		System.out.println("c언어"+c);
+		System.out.println("cpp언어"+cpp);
+		System.out.println("언어 평균"+average);
+	}
+	
+	public void SetJumsu(int c ,int cpp)
+	{
+		this.c=c;
+		this.cpp=cpp;
+		this.average=(c+cpp)/2.0f;
+	}
+
+	
+	
+}
