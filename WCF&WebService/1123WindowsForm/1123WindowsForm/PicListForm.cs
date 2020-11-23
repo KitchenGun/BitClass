0a775503bab895d33ad09a27605db8335e70343a
KitchenGun <kitchengun124@gmail.com>
2020-11-23 18:44:00 +0900

    WEB SERVICE를 사용한 사진 불러오기 및 간단한 계산기

---
+++ WCF&WebService/1123WindowsForm/1123WindowsForm/PicListForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _1123WindowsForm.localhost;
+
+namespace _1123WindowsForm
+{
+
+    public partial class PicListForm : Form
+    {
+
+        public string SelectedPic
+        {
+            get
+            {
+                return listBox1.SelectedItem.ToString();
+            }
+        }
+        // PictureService 객체를 하나 생성
+        PictureService pic = new PictureService();
+
+        public PicListForm()
+        {
+            InitializeComponent();
+            // 이미지 파일의 목록을 가져오는 메소드를 호출해서 문자열 배열에 저장한다.
+            string[] strPicList = pic.GetPictureList();
+            listBox1.DataSource = strPicList;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+
+   
+
+}
